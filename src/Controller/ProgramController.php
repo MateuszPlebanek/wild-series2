b8624586619e46a486a2fcc76efc67e1a2abeacd
MateuszPlebanek <matprocomm@gmail.com>
2025-10-14 12:36:00 +0200

    [FEAT]: ParamConverter + season/episode pages wired up

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -4,11 +4,12 @@ namespace App\Controller;
 
 use App\Entity\Program;
 use App\Entity\Season;
+use App\Entity\Episode;
 use App\Repository\ProgramRepository;
-use App\Repository\SeasonRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 
 #[Route('/program', name: 'program_')]
 class ProgramController extends AbstractController
@@ -19,38 +20,29 @@ class ProgramController extends AbstractController
         $programs = $programRepository->findAll();
 
         return $this->render('program/index.html.twig', [
-            'website' => 'Wild Series',
+            'website'  => 'Wild Series',
             'programs' => $programs,
         ]);
     }
-#[Route('/show/{id<^[0-9]+$>}', name: 'show')]
-    public function show(int $id, ProgramRepository $programRepository): Response
+
+    // 1) show(): ParamConverter sur {id} -> Program (cas simple)
+    #[Route('/show/{id<^[0-9]+$>}', name: 'show')]
+    public function show(Program $program): Response
     {
-        $program = $programRepository->findOneBy(['id' => $id]);
-
-        if (!$program) {
-            throw $this->createNotFoundException('No program with id : '.$id.' found in program\'s table.');
-        }
-
         return $this->render('program/show.html.twig', [
             'program' => $program,
         ]);
     }
-    #[Route('/{programId<\d+>}/seasons/{seasonId<\d+>}', name: 'season_show', methods: ['GET'])]
-    public function showSeason(
-        int $programId,
-        int $seasonId,
-        ProgramRepository $programRepository,
-        SeasonRepository $seasonRepository
-    ): Response {
-        $program = $programRepository->find($programId);
-        if (!$program) {
-            throw $this->createNotFoundException("Program #$programId introuvable");
-        }
 
-        $season = $seasonRepository->find($seasonId);
-        if (!$season || $season->getProgram()?->getId() !== $program->getId()) {
-            throw $this->createNotFoundException("Season #$seasonId n'appartient pas au Program #$programId");
+    // 2) showSeason(): ParamConverter avec MapEntity car placeholders != noms d'entités
+    // URL officielle du sujet : /program/{programId}/seasons/{seasonId}
+    #[Route('/{programId}/seasons/{seasonId}', name: 'season_show')]
+    public function showSeason(
+        #[MapEntity(mapping: ['programId' => 'id'])] Program $program,
+        #[MapEntity(mapping: ['seasonId' => 'id'])] Season $season
+    ): Response {
+        if ($season->getProgram() !== $program) {
+            throw $this->createNotFoundException('Cette saison n’appartient pas à ce programme.');
         }
 
         return $this->render('program/season_show.html.twig', [
@@ -59,4 +51,23 @@ class ProgramController extends AbstractController
         ]);
     }
 
+    // 3) showEpisode(): ParamConverter x3 avec MapEntity (placeholders ...Id)
+    // URL officielle du sujet :
+    // /program/{programId}/season/{seasonId}/episode/{episodeId}
+    #[Route('/{programId}/season/{seasonId}/episode/{episodeId}', name: 'episode_show')]
+    public function showEpisode(
+        #[MapEntity(mapping: ['programId' => 'id'])] Program $program,
+        #[MapEntity(mapping: ['seasonId'  => 'id'])] Season $season,
+        #[MapEntity(mapping: ['episodeId' => 'id'])] Episode $episode
+    ): Response {
+        if ($season->getProgram() !== $program || $episode->getSeason() !== $season) {
+            throw $this->createNotFoundException('Incohérence programme/saison/épisode.');
+        }
+
+        return $this->render('program/episode_show.html.twig', [
+            'program' => $program,
+            'season'  => $season,
+            'episode' => $episode,
+        ]);
+    }
 }
