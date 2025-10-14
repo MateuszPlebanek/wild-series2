cd112ffc1fd0208f1d52298bb3aabd59d492f56f
MateuszPlebanek <matprocomm@gmail.com>
2025-10-14 15:49:42 +0200

    [FEAT]: add forms for Category and Program

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,9 +2,13 @@
 // src/Controller/CategoryController.php
 namespace App\Controller;
 
+use App\Entity\Category; 
+use App\Form\CategoryType; 
 use App\Repository\CategoryRepository;
 use App\Repository\ProgramRepository;
+use Doctrine\ORM\EntityManagerInterface; 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request; 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -21,14 +25,33 @@ class CategoryController extends AbstractController
         ]);
     }
 
+    #[Route('/new', name: 'new', methods: ['GET','POST'])]
+    public function new(Request $request, EntityManagerInterface $em): Response
+    {
+        $category = new Category();
+
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($category);
+            $em->flush();
+
+            $this->addFlash('success', 'Category created!');
+            return $this->redirectToRoute('category_index');
+        }
+
+        return $this->render('category/new.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
     #[Route('/{categoryName}', name: 'show')]
     public function show(
         string $categoryName,
         CategoryRepository $categoryRepository,
         ProgramRepository $programRepository
-    ): Response
-    {
-        // 1) Récupérer la catégorie par son nom
+    ): Response {
         $category = $categoryRepository->findOneBy(['name' => $categoryName]);
 
         if (!$category) {
@@ -37,7 +60,6 @@ class CategoryController extends AbstractController
             );
         }
 
-        // 2) Récupérer au plus 3 Program, triés par id DESC, appartenant à cette catégorie
         $programs = $programRepository->findBy(
             ['category' => $category],
             ['id' => 'DESC'],
---
+++ src/Form/ProgramType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Program;
+use App\Entity\Category;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProgramType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'label' => 'Title',
+            ])
+            ->add('synopsis', TextareaType::class, [
+                'label' => 'Synopsis',
+            ])
+            ->add('poster', TextType::class, [
+                'label' => 'Poster URL',
+            ])
+            ->add('category', EntityType::class, [
+                'class'        => Category::class,
+                'choice_label' => 'name',   // affiche le name au lieu de l’id
+                'placeholder'  => 'Choose a category',
+                'label'        => 'Category',
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Program::class,
+        ]);
+    }
+}
